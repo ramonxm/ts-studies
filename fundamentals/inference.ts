6aaed00edb6ec6ff3746c98b0025e4f169038861
Ramon Xavier <ramon1234rxm@gmail.com>
2024-01-22 21:56:13 -0300

    adding examples to inference types

---
+++ fundamentals/inference.ts
@@ -0,0 +1,18 @@
+//  Inference
+
+// Defining variable with type inference
+{
+  let name = "Ramon";
+
+  let age = 23;
+
+  let programmer = true;
+
+  function sayHello(name: string) {
+    console.log(`Hello, i'm ${name}`);
+  }
+
+  class Person {
+    name: string = "Ramon";
+  }
+}
