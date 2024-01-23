bd786ed2338c344fd3fe7529807c0fafadb5a4a2
Ramon Xavier <ramon1234rxm@gmail.com>
2024-01-22 21:35:42 -0300

    initialize project and add primitive types examples

---
+++ fundamentals/primitive-types.ts
@@ -0,0 +1,20 @@
+// Primitive Types
+
+// Defining variable in string type
+const name: string = "Ramon";
+
+// Defining variable in number type
+const age: number = 23;
+
+// Defining variable in boolean type
+const programmer: boolean = true;
+
+// Defining function with string parameter and returns void
+function sayHello(name: string): void {
+  console.log(`Hello, i'm ${name}`);
+}
+
+// Defining class and assigning name property as string
+class Person {
+  name: string = "Ramon";
+}
